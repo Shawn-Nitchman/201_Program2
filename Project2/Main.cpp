db934568b9908f5dc7216de44b0104a4b2ed4ea2
Nitchman <smng47@umsystem.edu>
2020-03-11 19:00:47 -0500

    Creating student rooster program with multiple cpp files

---
+++ Project2/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "StudentClass.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Test";
+	return 0;
+}
---
+++ Project2/StudentClass.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#include "StudentClass.h"
+
+using namespace std;
